d163571b261e727c0553a1fa233fc59028858985
Aditya <aditya.kekare@gmail.com>
2021-12-01 21:53:38 -0500

    urls fixes

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,5 +1,5 @@
-const PROFILE_API = 'http://localhost:4000/api/profile';
-// const PROFILE_API = 'https://heroku-node-app-2021.herokuapp.com/' + 'api/profile';
+// const PROFILE_API = 'http://localhost:4000/api/profile';
+const PROFILE_API = 'https://heroku-node-app-2021.herokuapp.com/' + 'api/profile';
 
 export const getCurrentProfile = (dispatch) =>
     fetch(PROFILE_API)
